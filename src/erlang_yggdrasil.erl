bcb6154c93e2ce7c093eb032cf6b980ea85a1548
kushal kothari <kushalkothari285@gmail.com>
2020-07-08 11:27:52 +0000

    Update erlang_yggdrasil.erl

--- src/erlang_yggdrasil.erl
+++ src/erlang_yggdrasil.erl
@@ -1,30 +1,114 @@
- -module(yggdrasil_server).
-% -behaviour(gen_server).
-
--export([start/2, stop/1]).
-
--export([start_link/1]).
-
-% callbacks
-% -export([init/1, handle_call/3, handle_cast/2]).
 
 
-%% Start the Yggdrasil app. Opens a listen port at the port defined 
-%% and opens Acceptors number of concurrent acceptors.
-start(normal, _Args) ->
-    {ok, ListenSocket} = gen_tcp:listen(12345, [{active,once}, {reuseaddr, true}, inet6]),
-    {ok, Sup} = server_sup:start_link(ListenSocket),
-    _ = [ supervisor:start_child(Sup, []) || _ <- lists:seq(1,Acceptors)],
-    {ok, Sup, ListenSocket}.
+%
+% erlang_yggdrasil.erl
+% A yggdrasil server written in erlang which prints back to the user whatever
+% they sent. Whenever the user may want to exit, he/she has the
+% possibility of closing the connection by typing "exit".
+%
 
-%% Stop yggdrasil (closes the listen socket)
-stop(ListenSocket) ->
-    ok = gen_tcp:close(ListenSocket),
-    ok.
+-module(erlang_yggdrasil).
+-export([start_server/2, connect/1, recv_loop/1]).
+-compile(export_all).
 
 
-start_link(Socket) ->
-    gen_server:start_link(?MODULE, Socket, []).
+% The message that the users will receive after connecting.
+-define(WELCOME_MESSAGE, [
+  "\x1b[32mWelcome! This is a Yggdrasil server developed under Barrel Labs.
+You and I have memories longer than the road stretches out ahead\x1b[0m.\r\n"]).
 
+
+
+
+
+% What to prefix the users' input lines with.
+-define(LINE_PREFIX, "> ").
+
+
+
+
+%
+% start_server/2: starts listening for incoming connections on the port
+% add any Port number eg:12321 and your yggdrasil address in the place of "Yggdrasil" 
+
+
+start_server(Port,Yggdrasil) ->
+  {match,Check_yggdrasil} = re:run(Yggdrasil,"20"),
+  if
+      (Check_yggdrasil == [{0,2}]) -> {ok,Parsed_add} = inet:parse_address(Yggdrasil),  
+                                       spawn(fun () -> {ok, Listen} =  gen_tcp:listen(Port, [binary,inet6,{packet, raw},{nodelay, true},{reuseaddr, true},{active, once},{ip,Parsed_add}]),
+                                       connect(Listen)
+                                        end);
+      true -> io:format("Not a Yggdrasil address")
+   end. 
+
+
+
+
+
+% A small issue which would be corrected soon,You need to manually add your parsed yggdrasil address in the place of given IP address
+%todo: fix the issue of manually adding again the Yggdrasil address
+
+
+connect(Listen) ->
+  {ok, Socket} = gen_tcp:accept(Listen),
+  inet:setopts(Socket, [binary,inet6,{packet, raw},{nodelay, true},{reuseaddr, true},{active, once},{ip,{513,27209,16588,50190,56092,13264,47892,37868}}]), %add your own Yggdrasil address
+  spawn_link(erl_echo, connect, [Listen]),
+
+
+
+  gen_tcp:send(Socket, <<255, 252, 3>>),
+
+
+
+  gen_tcp:send(Socket, <<255, 252, 1>>),
+
+  gen_tcp:send(Socket, ?WELCOME_MESSAGE),
+  gen_tcp:send(Socket, ?LINE_PREFIX),
+  recv_loop(Socket),
+  gen_tcp:close(Socket).
+
+
+
+%
+% handle_data/2: handles data incoming from a connection -- checks
+% whether the user wants to close the session
+%
+
+
+handle_data(Socket, Data) ->
+  io:format("~p ~p ~p~n", [inet:peername(Socket), erlang:localtime(), Data]),
+  case Data of
+    % "exit\r\n" closes the session.
+    <<"exit\r\n">> ->
+      io:format("~p ~p Closed.~n", [inet:peername(Socket), erlang:localtime()]),
+      gen_tcp:close(Socket);
+
+    % IAC -- Interpret As Command.
+    <<255, _Rest/binary>> ->
+      % handle_negotiation(Socket, Data),
+      recv_loop(Socket);
+
+    % send back all other data received.
+    _ ->
+      gen_tcp:send(Socket, [Data, "> "]),
+      recv_loop(Socket)
+  end.
+
+
+
+
+
+%
+% recv_loop/1: handles a connection's event loop
+%
+recv_loop(Socket) ->
+  inet:setopts(Socket, [{active, once}]),
+  receive
+    {tcp, Socket, Data} ->
+      handle_data(Socket, Data);
+    {tcp_closed, Socket} ->
+      io:format("~p Client Disconnected.~n", [erlang:localtime()])
+  end.
 
 
